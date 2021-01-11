9e8656a88940164ebc020a74453eef512d344f22
sopanharithchea <csopanharith.student@puthisastra.edu.kh>
2021-01-11 18:57:02 +0700

    Updated and fixed image errors

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import Landing from "views/Lading.js";
+import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Login from "views/Login.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/landing" component={Landing} />
+      <Route path="/" component={Landing} />
       <Route path="/profile" component={Profile} />
       <Route path="/Login" component={Login} />
     </Switch>
